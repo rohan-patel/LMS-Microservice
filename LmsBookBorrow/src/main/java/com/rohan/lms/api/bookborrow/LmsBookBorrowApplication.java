57e5bc84edbccdb71d01a4b4a7f0162dd0c2b9d3
Rohan Patel <rohanpatel1531@gmail.com>
2023-01-10 23:17:05 +0530

    Created Skeleton for BorrowBook Microservice

---
+++ LmsBookBorrow/src/main/java/com/rohan/lms/api/bookborrow/LmsBookBorrowApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.lms.api.bookborrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsBookBorrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsBookBorrowApplication.class, args);
+	}
+
+}
---
+++ LmsBookBorrow/src/test/java/com/rohan/lms/api/bookborrow/LmsBookBorrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.lms.api.bookborrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsBookBorrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
