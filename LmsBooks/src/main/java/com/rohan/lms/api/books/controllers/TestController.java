b1a04940a251013c1c98854c6e5b8673111d77f6
Rohan Patel <rohanpatel1531@gmail.com>
2023-01-19 14:46:58 +0530

    Implemented Microservice inter-communication using Apache Artemis ActiveMQ

--- LmsBooks/src/main/java/com/rohan/lms/api/books/controllers/TestController.java
+++ LmsBooks/src/main/java/com/rohan/lms/api/books/controllers/TestController.java
@@ -1,41 +1,63 @@
 package com.rohan.lms.api.books.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import com.rohan.lms.api.books.model.Book;
 import com.rohan.lms.api.books.model.Error;
+import com.rohan.lms.api.books.repository.BookRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("/books")
 public class TestController {
-	
+
 	@Autowired
 	RestTemplate restTemplate;
 
+	@Autowired
+	private BookRepository bookrepo;
+
 	@GetMapping("/test")
-	public ResponseEntity test() {
-		
+	public ResponseEntity<Book> test(@RequestParam String isbn) {
+
 //		if (true) {
 //			throw new EntityNotFoundException("Hi there");
 //		}
-		HttpHeaders httpHeaders = new HttpHeaders();
-		HttpEntity<String> httpEntity = new HttpEntity<String>(httpHeaders);
-		
-		try {
-			return restTemplate.exchange("http://localhost:8082/exception-ws/entity-not-found?message=Hi there", HttpMethod.GET, httpEntity, ResponseEntity.class).getBody();
-		} catch (HttpStatusCodeException e) {
-			return ResponseEntity.status(e.getRawStatusCode()).body(e.getResponseBodyAs(Error.class));
-		}
+//		HttpHeaders httpHeaders = new HttpHeaders();
+//		HttpEntity<String> httpEntity = new HttpEntity<String>(httpHeaders);
+//		
+//		try {
+//			return restTemplate.exchange("http://localhost:8082/exception-ws/entity-not-found?message=Hi there", HttpMethod.GET, httpEntity, ResponseEntity.class).getBody();
+//		} catch (HttpStatusCodeException e) {
+//			return ResponseEntity.status(e.getRawStatusCode()).body(e.getResponseBodyAs(Error.class));
+//		}
+
+		Book book = bookrepo.findById(isbn).get();
+
+		List<String> author = bookrepo.findAuthorByIsbn(isbn);
+
+		List<String> genres = bookrepo.findGenreByIsbn(isbn);
+
+		Book book2 = new Book(isbn, book.getBookName(), genres, author, book.getYearOfPublication(), book.isBookAvailability(),
+				book.getAddedAt(), book.getUpdatedAt());
+
+		return new ResponseEntity<Book>(book2, HttpStatus.OK);
+
 	}
-	
+
 }
--- LmsBooks/src/main/java/com/rohan/lms/api/books/repository/BookRepository.java
+++ LmsBooks/src/main/java/com/rohan/lms/api/books/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.rohan.lms.api.books.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.rohan.lms.api.books.model.Book;
 
@@ -11,5 +12,11 @@ public interface BookRepository extends JpaRepository<Book, String> {
 	List<Book> findByAuthorName(String author);
 	
 	List<Book> findByBookGenre(String genre);
+	
+	@Query("SELECT b.authorName FROM Book b WHERE b.isbn = ?1")
+	List<String> findAuthorByIsbn(String isbn);
+	
+	@Query("SELECT b.bookGenre FROM Book b WHERE b.isbn = ?1")
+	List<String> findGenreByIsbn(String isbn);
 	
 }
